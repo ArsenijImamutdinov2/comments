2f4fe373e00669cc95e9ad038e3141a59a5f554f
Arsenij Imamutdinov <arsenijimamutdinov@yandex.ru>
2016-06-09 21:40:16 +0500

    2

--- src/main/resources/static/boo.js
+++ src/main/resources/static/boo.js
@@ -30,7 +30,12 @@ var CommentList = React.createClass({
 
 var CommentForm = React.createClass({
     getInitialState: function() {
-        return {author: '', text: '', authorList: [{name: "asdf"}, {name: "bghn"}]};
+        return {author: '', text: ''};
+    },
+    handleAuthorListChange: function(e) {
+        console.log("author list changed");
+        console.log(e.target.value);
+        this.setState({author: e.target.value});
     },
     handleAuthorChange: function(e) {
         this.setState({author: e.target.value});
@@ -48,10 +53,18 @@ var CommentForm = React.createClass({
         this.props.onCommentSubmit({authorName: author, text: text});
         this.setState({author: '', text: ''});
     },
+    chooseDefaultAuthor: function() {
+      if(this.props.authorList.length < 1) {
+          return "";
+      } else {
+          return this.props.authorList[0];
+      }
+    },
     render: function() {
-        var authors = this.state.authorList.map(function(author, idx){
+        var authors = this.props.authorList.map(function(author, idx){
+            //  <option selected={idx === 0} value={author.name}>{author.name}</option>
             return (
-                <option selected={idx === 0} value={author.name}>{author.name}</option>
+                <option value={author.name}>{author.name}</option>
             );
         });
         return (
@@ -62,7 +75,7 @@ var CommentForm = React.createClass({
                     value={this.state.author}
                     onChange={this.handleAuthorChange}
                     />
-                <select size="1">
+                <select size="1" value={this.chooseDefaultAuthor()}>
                     {authors}
                 </select>
                 <input
@@ -79,31 +92,44 @@ var CommentForm = React.createClass({
 
 var CommentBox = React.createClass({
     getInitialState: function() {
-        return {comments: []};
+        return {comments: [], authorList: []};
     },
     loadCommentsFromServer: function() {
         $.ajax({
-            url: this.props.url,
-
+            url: this.props.commentUrl,
             dataType: 'json',
             cache: false,
             success: function(response) {
                 this.setState({comments: response.comments});
             }.bind(this),
             error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
+                console.error(this.props.commentUrl, status, err.toString());
+            }.bind(this)
+        });
+    },
+    loadAuthorsFromServer: function() {
+        $.ajax({
+            url: this.props.authorUrl,
+            dataType: 'json',
+            cache: false,
+            success: function(response) {
+                this.setState({authorList: response});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.commentUrl, status, err.toString());
             }.bind(this)
         });
     },
     handleCommentSubmit: function(comment) {
         $.ajax({
-            url: this.props.url,
+            url: this.props.commentUrl,
             dataType: 'json',
             type: 'POST',
             contentType:'application/json',
             data: JSON.stringify(comment),
             success: function() {
                 this.loadCommentsFromServer();
+                this.loadAuthorsFromServer();
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -112,6 +138,7 @@ var CommentBox = React.createClass({
     },
     componentDidMount: function() {
         this.loadCommentsFromServer();
+        this.loadAuthorsFromServer();
         setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     },
     render: function() {
@@ -119,13 +146,13 @@ var CommentBox = React.createClass({
             <div className="commentBox">
                 <h1>Comments</h1>
                 <CommentList comments={this.state.comments} />
-                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} authorList={this.state.authorList} />
             </div>
         );
     }
 });
 
 ReactDOM.render(
-    <CommentBox url="/comments" pollInterval={2000}/>,
+    <CommentBox commentUrl="/comments" authorUrl="/authors" pollInterval={2000}/>,
     document.getElementById('example')
 );
